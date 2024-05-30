ccaa4a800d0f7bceff4296a490343cec3de50508
Batya Hershkovits <Batiah@skyvar.co.il>
2024-05-30 14:37:14 +0300

    try6

--- Asset-catalog/app/test_main.py
+++ Asset-catalog/app/test_main.py
@@ -6,16 +6,16 @@ runner = CliRunner()
 
 def test_upload_success():
     result = runner.invoke(app, ["{'filename':'GoodNight.txt','file':'Good Night!!'}"])
-    assert result.exit_code == 1
-    assert "Upload file is success!" in result.stdout
+    assert result.exit_code == 0
+    assert 'Upload file is success!' in result.stdout
     
 def test_file_exist():
     result = runner.invoke(app, ["{'filename':'Hello.txt','file':'Hello!!'}"])
     assert result.exit_code == 0
-    assert "Error: The file is exists!" in result.stdout
+    assert 'Error: The file is exists!' in result.stdout
 
 def test_upload_failed():
     result = runner.invoke(app, ["{'filename':'Hello2.txt'}"])
     assert result.exit_code == 0
-    assert "Upload file is failed!" in result.stdout
+    assert 'Upload file is failed!' in result.stdout
     
